35b599588e29b445a74015f01fa30b7584fca0d1
Christian Daniel Martinez Castillo <christiant10k22@gmail.com>
2023-09-24 02:42:09 -0600

    Initial commit

---
+++ src/main/java/com/chrizcode/s3/JwtDemoApplication.java
@@ -0,0 +1,13 @@
+package com.chrizcode.s3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chrizcode/s3/JwtDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrizcode.s3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
